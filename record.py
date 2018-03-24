a56c35c5c38a2cb142f2bbd04dff0ef45203030f
Ondřej Fuchs <o.fuchs@seznam.cz>
2018-03-24 17:54:39 +0100

    4

--- record.py
+++ record.py
@@ -1,5 +1,14 @@
 import argparse
-# Argumenty: sample/strema (sampling), number of channels (-c), format (-f), rate (-r), durantion
+from datetime import datetime
+import os, sys, time, errno
+import subprocess
+import platform
+import logging
+import shutil
+
+DEBUG = False
+# import alsaaudio, wave, numpy
+# Argumenty: sample/strema (sampling), number of channels (-c), format (-f), rate (-r), durantion (-d), period 
 # arecord -f S32_LE -c 2 -r 96000 -d 5 test.wav
 
 def check_rates(value):
@@ -8,26 +17,68 @@ def check_rates(value):
          raise argparse.ArgumentTypeError("%s is an invalid positive int value" % value)
     return ivalue
 
-
-parser = argparse.ArgumentParser(description='Parse default arguments for script')
-parser.add_argument("Sampling",type=str, nargs='?', default="SA", choices=['SA','ST'], 
+def parse_arguments():
+  parser = argparse.ArgumentParser(description='Parse default arguments for script')
+  parser.add_argument("Sampling",type=str, nargs='?', default="SA", choices=['SA','ST'], 
                     help="Setting style of recording.")
-parser.add_argument("Format", type=str, nargs='?', default="S32_LE", 
+  parser.add_argument("Format", type=str, nargs='?', default="S32_LE", 
                     choices=['S8', 'U8','S16_LE','S16_BE','U16_LE','U16_SE','S24_LE','S24_BE','U24_LE','U24_BE','S32_LE','S32_BE','U32_LE','U32_BE'], 
                     help="Set format of recording")
-parser.add_argument("Channel",type=int, nargs='?', default=2, choices=[1,2],
+  parser.add_argument("Channel",type=int, nargs='?', default=2, choices=[1,2],
                     help="Set number of channels.")
-parser.add_argument("Rate", type=check_rates, nargs='?', default=44000, 
+  parser.add_argument("Rate", type=check_rates, nargs='?', default=44000, 
                     help="Set rate of recording (Hz).")
-parser.add_argument("Durantion", type=int, nargs='?', default=5, 
+  parser.add_argument("Durantion", type=int, nargs='?', default=5, 
                     help="Set durantion of recording (s).")
+  parser.add_argument("Period", type=int, nargs='?', default=15, 
+                    help="Set period of recording (s).")
+  args = parser.parse_args()
+  
+  if DEBUG:
+    print args.Sampling
+    print args.Format
+    print args.Channel
+    print args.Rate
+    print args.Durantion
+    print args.Period
+  return args
 
-#parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
-#parser.add_argument('--sum', dest='accumulate', action='store_const',const=sum, default=max, help='sum the integers (default: find the max)')
+def make_subdir():
+  filePath = os.path.dirname(os.path.abspath( __file__ )) + "/recording/"
+  directory = os.path.dirname(filePath)
+  try:
+      os.makedirs(directory)
+  except OSError as e:
+      if e.errno != errno.EEXIST:
+          raise
 
-args = parser.parse_args()
-print args.Sampling
-print args.Format
-print args.Channel
-print args.Rate
-print args.Durantion
+def record(nameDevice, args):
+  timeNow = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+  record = 'arecord' + ' -f ' + args.Format + ' -c ' + str(args.Channel) + ' -r ' + str(args.Rate) + ' -d ' + str(args.Durantion) + ' ' +  \
+  "recording/" + timeNow + '-' + nameDevice + '.wav'
+  p = subprocess.Popen(record, shell=True)
+  time.sleep(args.Durantion)
+  p.terminate()
+  
+def main():
+
+  shutil.rmtree('recording')
+  
+  logging.basicConfig(level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(message)s")
+  
+  nameDevice = platform.node()
+  
+  args = parse_arguments()
+  
+  make_subdir()
+  #logging.debug("Pizza created")
+  while True:
+    record(nameDevice, args)
+    time.sleep(args.Period)
+      
+    
+# this is the standard boilerplate that calls the main() function
+if __name__ == '__main__':
+    # sys.exit(main(sys.argv)) # used to give a better look to exists
+    main()
+
