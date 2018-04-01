27c998f4c933e86801f9a27c5daa1fd1ffc15bea
RPI <rpi@rpi>
2018-04-01 22:27:27 +0000

    start uploader

--- record.py
+++ record.py
@@ -6,12 +6,20 @@ import subprocess
 import platform
 import logging
 import shutil
+import threading
+import glob
 from systemd.journal import JournaldLogHandler
+from random import choice
+from string import ascii_lowercase
+from string import digits
+
+# TODO: uploader
 
 # 172.16.187.99
-
 DEBUG = False
-# TODO: uploader
+# List of new records  
+global listOfFile
+listOfFile = []
 
 IN_BUFFER = bytearray(33600)
 
@@ -85,28 +93,31 @@ def parse_arguments():
 ###          
 # Funcion to make subdirectory to save records
 # 
-def make_subdir():
-  filePath = os.path.dirname(os.path.abspath( __file__ )) + "/recording/"
-  directory = os.path.dirname(filePath)
+def make_subdir(dirToSave, date):
+  directory = dirToSave + '/' + date 
   try:
       os.makedirs(directory)
   except OSError as e:
       if e.errno != errno.EEXIST:
           raise
+  return directory
+          
 ###          
 # Func to stard arecord to make record save in wav with parameters
 #  - Argumenty: sample/strema (sampling), number of channels (-c), format (-f), rate (-r), durantion (-d), period 
 # arecord -f S32_LE -c 2 -r 96000 -d 5 test.wav
-def record(dirToSave, nameDevice, args):
+def record(parentDirToSave, nameDevice, formatRecord, args):
+  global listOfFile
   timeNow = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-  timeD = args.Durantion 
-  record = 'arecord' + ' -f ' + args.Format + ' -c ' + str(args.Channel) + ' -r ' + str(args.Rate) + ' -d ' + str(args.Durantion) + ' ' +  \
-  dirToSave + '/' + timeNow + '-' + nameDevice + '.wav'
-  #"recording/" + timeNow + '-' + nameDevice + '.wav'
-  #record = 'rec -c 2 '+ "recording/" + timeNow + '-' + nameDevice + '.wav' + ' trim 0 ' + str(args.Durantion)
-  #'--file-type raw '
+  dirToSave = parentDirToSave + '/' + datetime.now().strftime('%Y-%m-%d')
+  randomString = (''.join(choice(digits) for i in range(6))) + '_' + (''.join(choice(ascii_lowercase) for i in range(8)))
+  nameOfFile = dirToSave + '/' + timeNow + '-' + randomString + '_' + nameDevice + formatRecord
+  
+  record = 'arecord' + ' -f ' + args.Format + ' -c ' + str(args.Channel) + ' -r ' + str(args.Rate) + ' -d ' + str(args.Durantion) + ' ' +  nameOfFile
+  # Start recording as subprocess
   p = subprocess.Popen(record, shell=True, bufsize=len(IN_BUFFER))
   logger.info('Record wav file into: %s',  dirToSave)
+  listOfFile.append(nameOfFile)
   p.wait()
   
 ###          
@@ -115,14 +126,43 @@ def record(dirToSave, nameDevice, args):
 def deleteData(dirToSave):
   fileList = os.listdir(dirToSave)
   for fileName in fileList:
-   os.remove(dirToSave+"/"+fileName)
-      
+    shutil.rmtree(dirToSave+"/"+fileName)
+
+###          
+# Function to upload new file to cloud
+#       
+def checkNewFileThread(actualDir, formatRecord, args, run_event):
+  global listOfFile
+  logger.info("Thread to upload data running.")
+  # By one loop after recording
+  time.sleep(args.Period)
+  while run_event.is_set():
+      time.sleep(args.Period)
+      # Remove after update
+      for fileName in listOfFile:
+        # Upload file (fileName) to cloud
+        #
+        # UPLOAD TO CLOAD
+        #
+        logger.info(fileName + " was upload.")
+        # Remove upload file from list of waiting file to upload
+        listOfFile.remove(fileName);
+        # Remove if upload was ok
+          #os.remove(fileName) 
+          #logger.info(fileName + " was remove.")
+
 ###          
 # Main function
 # 
 def main():
+  run_event = threading.Event()
+  run_event.set()
+  # Today date
+  date = str(datetime.now().strftime('%Y-%m-%d'))
   # Directory to save date
   dirToSave='/mnt/xfsdata'
+  # Format of records
+  formatRecord = '.wav'
   # Delete old data
   #if os.path.exists("recording"):
   #  shutil.rmtree('recording')
@@ -134,13 +174,23 @@ def main():
   # Parse arguments of device 
   args = parse_arguments()
   # Make dir to save records
-  #make_subdir()
+  actualDir = make_subdir(dirToSave, date)
+  # Make new thread to upload new record to cloud
+  UploadThread = threading.Thread(target=checkNewFileThread, args=[actualDir, formatRecord, args, run_event])
+  UploadThread.start()
   # Main loop to recordig
-  while True:
-    record(dirToSave, nameDevice, args)
-    if args.Mode == 'SA':
-      timeToSleep = args.Period - args.Durantion
-      time.sleep(timeToSleep)
+  try:
+    while True:
+      record(dirToSave, nameDevice, formatRecord, args)
+      if args.Mode == 'SA':
+        timeToSleep = args.Period - args.Durantion
+        time.sleep(timeToSleep)
+  # Join check thread
+  except KeyboardInterrupt:
+    logger.info("Attempting to close threads.")
+    run_event.clear()
+    UploadThread.join()
+    logger.info("Threads successfully closed.")
       
 # this is the standard boilerplate that calls the main() function
 if __name__ == '__main__':
